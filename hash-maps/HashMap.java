cbcb5206669434a3a9b1d24e03fdaaad1d3ed010
Prashant <53073860+prashantsingh003@users.noreply.github.com>
2022-10-19 05:13:27 +0530

    driver-code-hashmap

--- hash-maps/HashMap.java
+++ hash-maps/HashMap.java
@@ -31,12 +31,12 @@ public class HashMap <K,V>{
         if(list_i==-1){
             HMnode node=new HMnode(key, value);
             this.buckets[bucket_i].add(node);
-            System.out.println("Insert: "+key);
+            System.out.println("Inserted: "+key);
             this.size++;
         }
         else{
             this.buckets[bucket_i].get(list_i).value= value;
-            System.out.println("Update: "+key);
+            System.out.println("Updated: "+key);
         }
 
         double lambda= this.size*1.0/this.buckets.length;
@@ -52,13 +52,13 @@ public class HashMap <K,V>{
         return getListIndex(key, bucket_i)!=-1;
     }
 
-    public V get(K key) throws Exception{
+    public V get(K key){
         int bucket_i= getBucketIndex(key);
         int list_i=getListIndex(key, bucket_i);
         return list_i!=-1 ? this.buckets[bucket_i].get(list_i).value : null;
     }
 
-    public V remove(K key) throws Exception{
+    public V remove(K key){
         int bucket_i= getBucketIndex(key);
         int list_i=getListIndex(key, bucket_i);
         if(list_i==-1)
@@ -112,11 +112,90 @@ public class HashMap <K,V>{
         }
     }
 
+    // DRIVER FUNCTION
     public static void main(String[] args) {
-        HashMap h=new HashMap<>();
-        for(int i=0;i<15;i++){
-            h.put("asd"+i, i);
+        Scanner sc=new Scanner(System.in);
+        HashMap hash=new HashMap<>();
+        
+        System.out.println("The key string + index will be the final key used and the value will be the index itself\nEnter a string for key:");
+        String key=sc.next();
+        System.out.println("Please enter the number of elements to be inserted: ");
+        int size=sc.nextInt();
+
+        for(int i=0;i<size;i++){
+            hash.put(key+i, i);
         }
-        System.out.println(h.size);
+        System.out.println("""
+                KEYS\tOPERATIONS
+                1\tAdd or Update Element
+                2\tCheck Key existance
+                3\tGet
+                4\tRemove
+                5\tGet all keys
+                6\tGet all values
+                7\tGet complete data
+                9\tView Commands
+                0\tQuit
+                """);
+        int input=9,key_i,value;
+        while(input!=0){
+            input=sc.nextInt();
+            switch(input){
+                case(1):
+                    System.out.print("\tEnter Value to add or update\t");
+                    hash.put(key+size++, sc.nextInt());
+                    break;
+                case(2):
+                    System.out.println("\tEnter Key index to check\t");
+                    key_i=sc.nextInt();
+                    System.out.println(hash.containsKey(key+key_i));
+                    break;
+                case(3):
+                    System.out.println("\tEnter Key index to get\t");
+                    key_i=sc.nextInt();
+                    System.out.println(hash.get(key+key_i));
+                    break;
+                case(4):
+                    System.out.println("\tEnter Key index to remove\t");
+                    key_i=sc.nextInt();
+                    System.out.println(hash.remove(key+key_i));
+                    break;
+                case(5):
+                System.out.println("/tNot ready yet\t");
+                    // for(String s:hash.getKeys()){
+                    //     System.out.print(s);
+                    //     System.out.print('\t');
+                    // }
+                    break;
+                case(6):
+                System.out.println("/tNot ready yet\t");
+                    // for(String s:hash.getValues()){
+                    //     System.out.print(s);
+                    //     System.out.print('\t');
+                    // }
+                    break;
+                case(7):
+                    System.out.println("/tNot ready yet\t");
+                    break;
+                case(9):
+                    System.out.println("""
+                        KEYS\tOPERATIONS
+                        1\tAdd or Update Element
+                        2\tCheck Key existance
+                        3\tGet
+                        4\tRemove
+                        5\tGet all keys
+                        6\tGet all values
+                        7\tGet complete data
+                        9\tView Commands
+                        0\tQuit
+                        """);
+                    break;
+                default:
+                    System.out.println("Invalid input\t");
+                    break;
+            }
+        }
+        sc.close();
     }
 }
